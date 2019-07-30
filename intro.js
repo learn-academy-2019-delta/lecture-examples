78fbaf5ed77bfdce9eac006217828f976076a36d
Sarah Proctor <sproctor950@gmail.com>
2019-07-29 18:34:03 -0700

    js intro notes

---
+++ intro.js
@@ -0,0 +1,35 @@
+// INTRO TO JAVASCRIPT LECTURE NOTES: 7/29/19
+
+// JavaScript – lightweight programming language (small memory footprint, doesn’t take up a lot of server energy), dynamic, loosely typed (don’t have to declare the data type and can change a variable to a different type without issue),  (vs. static/strong where a data type is declared for each variable upon declaration like C, Java), uses camelCase, JS is an interpreted (line by line) language (vs. complied which processes the source code to machine code)
+
+// Number operators
+console.log(6 + 7)
+console.log(8 ** 6)
+console.log(8 % 7)
+
+// String operators
+console.log("hello delta")
+console.log("6 + 7")
+console.log("hello delta".length)
+
+
+// Negation with a console log label 
+console.log("negation", !true)
+
+// Concatenation
+console.log("hello" + " delta")
+
+// Type coercion
+console.log("6" === 3+3)
+console.log(" " + 1)
+
+
+// Variables
+var myName = "Sarah"
+console.log(myName.length)
+var yourName = "Dante"
+console.log(myName + yourName)
+
+// Reassigning a variable
+myName = 78493
+console.log(myName)
